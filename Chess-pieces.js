3dca0181e2e5cc399fe2eedf57a62ccb9dee9599
DanielChernyak <102975651+DanielChernyak@users.noreply.github.com>
2022-04-18 17:36:03 +0300

    Update Chess-pieces.js

--- Chess-pieces.js
+++ Chess-pieces.js
@@ -16,40 +16,24 @@ class Piece {
 
 function getInitialBoard() {
   let result = [];
-  result.push(new Piece(0, 0, "rook", DARK_PLAYER))
-  result.push(new Piece(0, 1, "knight", DARK_PLAYER))
-  result.push(new Piece(0, 2, "bishop", DARK_PLAYER))
-  result.push(new Piece(0, 3, "queen", DARK_PLAYER))
-  result.push(new Piece(0, 4, "king", DARK_PLAYER))
-  result.push(new Piece(0, 5, "bishop", DARK_PLAYER))
-  result.push(new Piece(0, 6, "knight", DARK_PLAYER))
-  result.push(new Piece(0, 7, "rook", DARK_PLAYER))
-  result.push(new Piece(1, 0, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 1, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 2, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 3, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 4, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 5, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 6, "pawn", DARK_PLAYER))
-  result.push(new Piece(1, 7, "pawn", DARK_PLAYER))
-  result.push(new Piece(6, 0, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 1, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 2, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 3, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 4, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 5, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 6, "pawn", WHITE_PLAYER))
-  result.push(new Piece(6, 7, "pawn", WHITE_PLAYER))
-  result.push(new Piece(7, 0, "rook", WHITE_PLAYER))
-  result.push(new Piece(7, 1, "knight", WHITE_PLAYER))
-  result.push(new Piece(7, 2, "bishop", WHITE_PLAYER))
-  result.push(new Piece(7, 3, "queen", WHITE_PLAYER))
-  result.push(new Piece(7, 4, "king", WHITE_PLAYER))
-  result.push(new Piece(7, 5, "bishop", WHITE_PLAYER))
-  result.push(new Piece(7, 6, "knight", WHITE_PLAYER))
-  result.push(new Piece(7, 7, "rook", WHITE_PLAYER))
+  addPieces(result, 0, DARK_PLAYER);
+  addPieces(result, 7, WHITE_PLAYER);
 
-  return result;
+  for (let i = 0; i < BOARD_SIZE ; i++) {
+    result.push(new Piece(1, i, "pawn", DARK_PLAYER))
+    result.push(new Piece(6, i, "pawn", WHITE_PLAYER))
+  }
+return result;
+}
+function addPieces(result, row, player) {
+  result.push(new Piece(row, 0, "rook", player))
+  result.push(new Piece(row, 1, "knight", player))
+  result.push(new Piece(row, 2, "bishop", player))
+  result.push(new Piece(row, 3, "queen", player))
+  result.push(new Piece(row, 4, "king", player))
+  result.push(new Piece(row, 5, "bishop", player))
+  result.push(new Piece(row, 6, "knight", player))
+  result.push(new Piece(row, 7, "rook", player))
 }
 
 function addImage(cell, player, name) {
