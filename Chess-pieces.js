108a5ea0dd1bff805227a3f020f6b7937f860b2f
DanielChernyak <102975651+DanielChernyak@users.noreply.github.com>
2022-04-18 16:53:53 +0300

    Update Chess-pieces.js

--- Chess-pieces.js
+++ Chess-pieces.js
@@ -1,34 +1,83 @@
 const BOARD_SIZE = 8;
-const WHITE_TYPE = 'white';
-const DARK_TYPE = 'dark_rotated';
+const WHITE_PLAYER = 'white';
+const DARK_PLAYER = 'dark_rotated';
 
 let selectedCell;
+let pieces = [];
 
-function addImage(cell, type, name) {
+class Piece {
+  constructor(row, col, type, player) {
+    this.row = row;
+    this.col = col;
+    this.type = type;
+    this.player = player;
+  }
+}
+
+function getInitialBoard() {
+  let result = [];
+  result.push(new Piece(0, 0, "rook", DARK_PLAYER))
+  result.push(new Piece(0, 1, "knight", DARK_PLAYER))
+  result.push(new Piece(0, 2, "bishop", DARK_PLAYER))
+  result.push(new Piece(0, 3, "queen", DARK_PLAYER))
+  result.push(new Piece(0, 4, "king", DARK_PLAYER))
+  result.push(new Piece(0, 5, "bishop", DARK_PLAYER))
+  result.push(new Piece(0, 6, "knight", DARK_PLAYER))
+  result.push(new Piece(0, 7, "rook", DARK_PLAYER))
+  result.push(new Piece(1, 0, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 1, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 2, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 3, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 4, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 5, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 6, "pawn", DARK_PLAYER))
+  result.push(new Piece(1, 7, "pawn", DARK_PLAYER))
+  result.push(new Piece(6, 0, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 1, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 2, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 3, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 4, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 5, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 6, "pawn", WHITE_PLAYER))
+  result.push(new Piece(6, 7, "pawn", WHITE_PLAYER))
+  result.push(new Piece(7, 0, "rook", WHITE_PLAYER))
+  result.push(new Piece(7, 1, "knight", WHITE_PLAYER))
+  result.push(new Piece(7, 2, "bishop", WHITE_PLAYER))
+  result.push(new Piece(7, 3, "queen", WHITE_PLAYER))
+  result.push(new Piece(7, 4, "king", WHITE_PLAYER))
+  result.push(new Piece(7, 5, "bishop", WHITE_PLAYER))
+  result.push(new Piece(7, 6, "knight", WHITE_PLAYER))
+  result.push(new Piece(7, 7, "rook", WHITE_PLAYER))
+
+  return result;
+}
+
+function addImage(cell, player, name) {
   const image = document.createElement('img');
-  image.src = 'images/' + type + '/' + name + '.png';
+  image.src = 'images/' + player + '/' + name + '.png';
   cell.appendChild(image);
 }
 
-function addImageByIndex(cell, type, index) {
+function addImageByIndex(cell, player, index) {
   if (index === 0 || index === 7) {
-    addImage(cell, type, 'rook');
+    addImage(cell, player, 'rook');
   } else if (index === 1 || index === 6) {
-    addImage(cell, type, 'knight');
+    addImage(cell, player, 'knight');
   } else if (index === 2 || index === 5) {
-    addImage(cell, type, 'bishop');
+    addImage(cell, player, 'bishop');
   } else if (index === 3) {
-    addImage(cell, type, 'queen');
+    addImage(cell, player, 'king');
   } else if (index === 4) {
-    addImage(cell, type, 'king');
+    addImage(cell, player, 'queen');
   }
 }
+
 function onCellClick(event) {
   if (selectedCell !== undefined) {
     selectedCell.classList.remove('selected');
   }
- selectedCell = event.currentTarget;
- selectedCell.classList.add('selected');
+  selectedCell = event.currentTarget;
+  selectedCell.classList.add('selected');
 }
 
 function createChessBoard() {
@@ -45,17 +94,13 @@ function createChessBoard() {
         cell.className = 'dark-cell';
       }
       cell.addEventListener('click', onCellClick);
-
-      if (i === 0) {
-        addImageByIndex(cell, DARK_TYPE, j);
-      } else if (i === 1) {
-        addImage(cell, DARK_TYPE, 'pawn');
-      } else if (i === 6) {
-        addImage(cell, WHITE_TYPE, 'pawn');
-      } else if (i === 7) {
-        addImageByIndex(cell, WHITE_TYPE, j);
-      }
     }
   }
+  pieces = getInitialBoard();
+
+  for (let piece of pieces) {
+    addImage(table1.rows[piece.row].cells[piece.col], piece.player, piece.type);
+  }
 }
+
 window.addEventListener('load', createChessBoard);
